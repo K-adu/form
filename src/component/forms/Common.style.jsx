15116e24eab80d4f237b4b35930c544294a1681d
Nabin <nabin.pandey@ebpearls.com>
2023-09-06 09:22:06 +0545

    styled component

---
+++ src/component/forms/Common.style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const VerticalStack = styled.div`
+  padding: 8px;
+  display: flex;
+  gap: 6px;
+  flex-direction: column;
+`;
--- src/component/forms/UserInfo.jsx
+++ src/component/forms/UserInfo.jsx
@@ -1,8 +1,9 @@
-import { TextField, Button } from '@mui/material';
-import { useFormik, Field, Form, ErrorMessage } from 'formik';
-import { useContext } from 'react';
-import * as yup from 'yup';
-import { FormContext } from '../../App';
+import { TextField, Button } from "@mui/material";
+import { useFormik, Field, Form, ErrorMessage } from "formik";
+import { useContext } from "react";
+import * as yup from "yup";
+import { FormContext } from "../../App";
+import { VerticalStack } from "./Common.style";
 export default function UserInfo() {
   const { activeStepIndex, setActiveStepIndex, formData, setFormData } =
     useContext(FormContext);
@@ -14,66 +15,68 @@ export default function UserInfo() {
   });
   const Formik = useFormik({
     initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      age: '',
+      firstName: "",
+      lastName: "",
+      email: "",
+      age: "",
     },
     validationSchema: ValidationSchema,
     onSubmit: (values) => {
       const data = { ...formData, ...values };
       setFormData(data);
       setActiveStepIndex(activeStepIndex + 1);
-      console.log('activeStepIndex');
+      console.log("activeStepIndex");
     },
   });
   return (
     <div>
       <form onSubmit={Formik.handleSubmit}>
-        <TextField
-          fullWidth
-          id="firstName"
-          name="firstName"
-          label="firstName"
-          value={Formik.values.firstName}
-          onChange={Formik.handleChange}
-          onBlur={Formik.handleBlur}
-          error={Formik.touched.firstName && Boolean(Formik.errors.firstName)}
-        />
-        <TextField
-          fullWidth
-          id="lastName"
-          name="lastName"
-          label="lastName"
-          value={Formik.values.lastName}
-          onChange={Formik.handleChange}
-          onBlur={Formik.handleBlur}
-          error={Formik.touched.lastName && Boolean(Formik.errors.lastName)}
-        />
+        <VerticalStack>
+          <TextField
+            fullWidth
+            id="firstName"
+            name="firstName"
+            label="firstName"
+            value={Formik.values.firstName}
+            onChange={Formik.handleChange}
+            onBlur={Formik.handleBlur}
+            error={Formik.touched.firstName && Boolean(Formik.errors.firstName)}
+          />
+          <TextField
+            fullWidth
+            id="lastName"
+            name="lastName"
+            label="lastName"
+            value={Formik.values.lastName}
+            onChange={Formik.handleChange}
+            onBlur={Formik.handleBlur}
+            error={Formik.touched.lastName && Boolean(Formik.errors.lastName)}
+          />
 
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="email"
-          value={Formik.values.email}
-          onChange={Formik.handleChange}
-          onBlur={Formik.handleBlur}
-          error={Formik.touched.email && Boolean(Formik.errors.email)}
-        />
-        <TextField
-          fullWidth
-          id="age"
-          name="age"
-          label="age"
-          value={Formik.values.age}
-          onChange={Formik.handleChange}
-          onBlur={Formik.handleBlur}
-          error={Formik.touched.age && Boolean(Formik.errors.email)}
-        />
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Next
-        </Button>
+          <TextField
+            fullWidth
+            id="email"
+            name="email"
+            label="email"
+            value={Formik.values.email}
+            onChange={Formik.handleChange}
+            onBlur={Formik.handleBlur}
+            error={Formik.touched.email && Boolean(Formik.errors.email)}
+          />
+          <TextField
+            fullWidth
+            id="age"
+            name="age"
+            label="age"
+            value={Formik.values.age}
+            onChange={Formik.handleChange}
+            onBlur={Formik.handleBlur}
+            error={Formik.touched.age && Boolean(Formik.errors.email)}
+          />
+          <Button color="primary" variant="contained" fullWidth type="submit">
+            Next
+          </Button>
+        </VerticalStack>
       </form>
     </div>
   );
